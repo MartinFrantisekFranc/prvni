a63ec9d07af2e2b4998f66e98d02573591ccd003
MartinFrantisekFranc <franc.m@seznam.cz>
2025-09-17 16:47:27 +0200

    Nazdar

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Ahoj GitHub!");
         System.out.println("AHOJ");
+        System.out.println("Nazdar");
     }
 }
